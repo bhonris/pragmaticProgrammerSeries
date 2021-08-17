bf2c5e068f1ac5f0a02ed70d6e625843dfe5e9c1
Bhon Bunnag <bhonris@gmail.com>
2021-08-16 21:42:03 -0700

    new example

--- 001-orthogonality.js
+++ 001-orthogonality.js
@@ -37,8 +37,8 @@ function badFunction2(num){
 
 // Let's say you can only call each of these functions once. How do you set 
 // a = 1 and b = 5
-const num1 = undefined; // add value here INTEGERS ONLY
-const num2 = undefined; // add value here INTEGERS ONLY
+const num1 = 3; // add value here INTEGERS ONLY
+const num2 = -1; // add value here INTEGERS ONLY
 badFunction1(num1)
 badFunction2(num2)
 console.log(`Expect value of a to equal 1. Received:  ${a}`)
@@ -53,7 +53,7 @@ console.log(`Expect value of b to equal 5. Received:  ${b}`)
 //
 // num1 = 3; num2 = -1
 
-// On the other hand an exmaple of orthogonal coding
+// On the other hand an example of orthogonal coding
 let c = 0;
 let d = 0;
 
@@ -69,11 +69,74 @@ function goodFunction2(num){
 // c = 420 and d = 69
 const num3 = undefined; // add value here INTEGERS ONLY
 const num4 = undefined; // add value here INTEGERS ONLY
-goodFunction1(num3)
-goodFunction2(num4)
+goodFunction1(420)
+goodFunction2(69)
 console.log(`Expect value of c to equal 420. Received:  ${c}`)
 console.log(`Expect value of d to equal 69. Received:  ${d}`)
 // Answer is trivial
 
 
 // For a more applicable problem
+const status = {
+  loadingA: false,
+  loadingB: false
+}
+
+async function sleep(ms) {
+  return new Promise(resolve => setTimeout(resolve, ms));
+}
+function loadingComponent(ms) {
+  return new Promise(resolve => setTimeout(resolve, ms));
+}
+
+async function loadA() {
+  console.log("Start loadA");
+  status.loadingA = true;
+  await loadingComponent(Math.floor(Math.random() * 3000));
+  status.loadingA = false;
+  console.log("Finish loadA");
+}
+
+async function loadB() {
+  console.log("Start loadB");
+  status.loadingB = true;
+  await loadingComponent(Math.floor(Math.random() * 3000));
+  status.loadingB = false;
+  console.log("Finish loadB");
+}
+
+function allowUserInteraction(){
+  return !status.loadingA && !status.loadingB
+}
+async function waitToFinishLoading(){
+  console.log("wait for finish loading");
+  while(!allowUserInteraction()){
+    await sleep(500)
+  }
+  console.log("Allowing user interaction");
+
+}
+loadB();
+loadA();
+waitToFinishLoading();
+
+
+
+// function sleep(ms) {
+//   return new Promise(resolve => setTimeout(resolve, ms));
+// }
+
+// async function demo() {
+//   console.log('Taking a break...');
+//   await sleep(2000);
+//   console.log('Two seconds later, showing sleep in a loop...');
+
+//   // Sleep in loop
+//   for (let i = 0; i < 5; i++) {
+//     if (i === 3)
+//       await sleep(2000);
+//     console.log(i);
+//   }
+// }
+
+// demo();
