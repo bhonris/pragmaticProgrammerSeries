a6e7852b885e7e99c4153ac20b6855347fbd79eb
Bhon Bunnag <bhonris@gmail.com>
2021-08-23 21:01:44 -0700

    rearrangement

---
+++ problem_set/001-array_manipulation_and_recursion.js.js
@@ -0,0 +1,39 @@
+const numbers = [2,5,4,8,9,11,10]
+// 1 
+// Map and Filter Review 
+// Consumes a list of numbers
+// And selects only the numbers that are prime
+// Then double all those numbers
+// Returns a list
+function doublePrimeNumbers(anArray){
+  return []
+}
+const processedNumbers = doublePrimeNumbers(numbers)
+// Expected Value: [4, 10, 22]
+
+
+// 2
+// Reduce 
+// Use the javascript  built-in function 'reduce' to find the max value of a list
+function findMax(anArray){
+  return 0
+}
+const maxNumber = findMax(anArray)
+// Expected Value 11
+
+
+// 3 
+// Recursion
+// Fibonacci
+// Create a function that consumes a number, and returns the fibonacci number of that order
+function getFibonacci(n){
+
+}
+// getFibonacci(0)  => 0
+// getFibonacci(1)  => 1
+// getFibonacci(2)  => 1
+// getFibonacci(3)  => 2
+// getFibonacci(4)  => 3
+// getFibonacci(5)  => 5
+// getFibonacci(11) => 89
+
