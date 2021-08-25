a5dcb7208eec6bad95357946edde1dbbace0f4d6
Bhon Bunnag <bhonris@gmail.com>
2021-08-24 19:56:48 -0700

    derp

---
+++ problem_set/001-FizzBuzz.js
@@ -0,0 +1,21 @@
+// Fizzbuzz
+// input type: int
+// output: string
+
+function getFizzBuzzNumber(num){
+  return false
+}
+
+// if num is divisible by 3, output fizz
+// if num is divisible by 5, output buzz
+// if num is divisible by 3 and 5, output fizzbuzz
+
+// getFizzBuzzList
+// input type: int
+// output: array
+function getFizzBuzzList(num){
+  return []
+}
+// Example:
+// getFizzBuzzList(4) => [1, 2, fizz, 4]
+// get FizzBuzzList(10) => [1,2,fizz,4, buzz, fizz, 7, 8, fizz, buzz]
