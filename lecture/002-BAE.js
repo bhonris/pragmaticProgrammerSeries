f11a05180a7d1ec0edf70dc3f4c3b09aea08119a
Bhon Bunnag <bhonris@gmail.com>
2021-09-05 21:07:16 -0700

    problem set 3

--- lecture/002-BAE.js
+++ lecture/002-BAE.js
@@ -26,30 +26,45 @@ for(let i = 1; i <= 1; i ++){
 const FRUIT = [
     {name: "apple", cost: 10},
     {name: "pear", cost: 20},
-    {name: "banana", cost: 30}
+    {name: "banana", cost: 30},
+    {name: "", cost: -1} 
 ]
 
 const VEGETABLES = [
     {name: "lettuce", cost: 5},
     {name: "tomato", cost: 15},
-    {name: "onion", cost: 25}
+    {name: "onion", cost: 25},
+    {name: "", cost: -1}
 ]
+
+function sanityCheck(cost, name){
+    return cost <= 0 || name === ""
+}
+
 function insertIntoFruitTable(name, cost){
-    if(cost <= 0){
-        return
-    }
-    if(name === ""){
-        return
-    }
+    if(sanityCheck(cost,name)) return
     VEGETABLES.push({name, cost})
 }
 
+function derp(e) {
+    e.name = "asdf"
+}
+
 function insertIntoVegetableTable(name, cost){
-    if(cost <= 0){
-        return
-    }
-    if(name === ""){
-        return
-    }
+    if(sanityCheck(cost,name)) return
     FRUIT.push({name, cost})
+}
+
+function checkIfFruitTableIsOkay(){
+    FRUIT.forEach(e => {
+        derp(e)
+    })
+}
+checkIfFruitTableIsOkay()
+console.log(FRUIT)
+
+function checkIfVegetableTableIsOkay(){
+    VEGETABLES.forEach(e => {
+        // name: NOT FOUND, cost: 0
+    })
 }
---
+++ problem_set/003-problem-set.js
@@ -0,0 +1,19 @@
+var assert = require('assert')
+
+
+// Problem 3
+// The following exists as denomiation of the Thai baht:
+// 1b, 2b, 5b, 10b, 20b, 50b
+// How many different ways are there to sum a total of 50 baht?
+
+// EXAMPLE: 10x1b + 5x2b + 2x5b + 2x10b = 50b
+// HINT: Use recursion
+
+// Create a function that evalutes that information
+function getNumberOfVariations(){
+    const sum = 50
+    const denominations = [1,2,5,10,20,50]
+    // ...
+    return -1
+}
+
