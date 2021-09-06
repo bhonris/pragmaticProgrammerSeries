51bb6b4a299e38d7169e11f8353a1c698a26f6c6
Bhon Bunnag <bhonris@gmail.com>
2021-09-05 21:54:41 -0700

    More problems to set 3

--- problem_set/003-problem-set.js
+++ problem_set/003-problem-set.js
@@ -1,7 +1,34 @@
 var assert = require('assert')
 
+// Problem 1a
+// More practice with MAP/FILTER
+// IF gender = M, add Mr.
+// If gender = F, add Ms.
+DATA = [
+    {name: "Bob", gender: "M", age: 14},
+    {name: "Sally", gender: "F", age: 20},
+    {name: "Jack", gender: "M", age: 26},
+    {name: "David", gender: "M", age: 69},
+    {name: "Anna", gender: "F", age: 32},
+]
 
-// Problem 3
+function getHonorifics(){
+    const newArray = "..."
+    return newArray
+}
+// RESULT
+// console.log(getHonorifics()) => ["Mr. Bob", "Ms. Sally", "Mr. Jack", "Mr. David", "Ms. Anna"]
+
+// Problem 1b
+// More practice with MAP/FILTER
+// Get only if gender= "M" and age > 18
+function getAdultMen(){
+    const newArray = "..."
+    return newArray
+}
+// console.log(getHonorifics()) => [{name: "Jack", gender: "M", age: 26}, {name: "David", gender: "M", age: 69}]
+
+// Problem 2
 // The following exists as denomiation of the Thai baht:
 // 1b, 2b, 5b, 10b, 20b, 50b
 // How many different ways are there to sum a total of 50 baht?
@@ -16,4 +43,3 @@ function getNumberOfVariations(){
     // ...
     return -1
 }
-
